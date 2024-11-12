e28262ab0e33a1989e8a18147cff0ee27a900645
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-12 09:07:56 +0100

    Initial commit

---
+++ FocusBubble/FocusBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBubbleApp.swift
+//  FocusBubble
+//
+//  Created by Jan Armbrust on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusBubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
