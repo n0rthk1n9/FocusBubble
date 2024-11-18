ff0ec4acaf3a9f488b3b67bc170d88e016d90693
Daniele Fontana <danyfo00@gmail.com>
2024-11-18 09:43:17 +0100

    Bubble Animation in StatisticsView (#10)

---
+++ FocusBubble/Views/BubbleAnimationView.swift
@@ -0,0 +1,36 @@
+//
+//  BubbleAnimationView.swift
+//  FocusBubble
+//
+//  Created by Daniele Fontana on 15/11/24.
+//
+
+import SwiftUI
+
+struct BubbleAnimationView: View {
+    var imageName: String
+    var animationDuration: Double = 1.5
+    var maxOffset: CGFloat = 10
+
+    @State private var xOffset: CGFloat = 0
+    @State private var yOffset: CGFloat = 0
+
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140, height: 140)
+            .offset(x: xOffset, y: yOffset)
+            .onAppear {
+               
+                withAnimation(.easeInOut(duration: animationDuration).repeatForever(autoreverses: true)) {
+                    xOffset = CGFloat.random(in: -maxOffset...maxOffset)
+                    yOffset = CGFloat.random(in: -maxOffset...maxOffset)
+                }
+            }
+    }
+}
+
+#Preview {
+    BubbleAnimationView(imageName: "Bubble100")
+}
